1fe56544fa624936e76385f546d4c11692afb098
Chang-soo Han <are@spoqa.com>
2018-08-15 16:40:38 +0900

    Apply material-ui theme provider

--- nekoyume/static/src/components/App/index.tsx
+++ nekoyume/static/src/components/App/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme  from '@material-ui/core/styles/createMuiTheme';
+
 class App extends React.Component {
 
     render() {
         return (
-            'Hello world!'
+            <MuiThemeProvider theme={createMuiTheme()}>
+                Hello world!
+            </MuiThemeProvider>
         );
     }
 }
