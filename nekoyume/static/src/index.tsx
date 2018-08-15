805e05dfafea5b181ec2e3d3b0920f12b5ac6801
Chang-soo Han <are@spoqa.com>
2018-08-15 17:48:00 +0900

    Add translations initializing

--- nekoyume/static/src/index.tsx
+++ nekoyume/static/src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from 'components/App';
+import { initializeI18n } from 'utils/translations';
 
 const container = document.getElementById('root');
 
-const run = () => {
+const run = async () => {
     if (module.hot) {
         module.hot.accept('./components/App', () => {
             setImmediate(() => {
@@ -15,6 +16,11 @@ const run = () => {
         });
     }
 
+    try {
+        await initializeI18n('/translations/');
+    } catch {
+        console.warn('Failed to load translations.');
+    }
     ReactDOM.render(<App />, container);
 };
 
