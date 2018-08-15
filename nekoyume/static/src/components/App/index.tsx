789fb2437dd8a97abd9db2667815438c7abce5dc
Chang-soo Han <are@spoqa.com>
2018-08-15 16:47:29 +0900

    Add react-router

--- nekoyume/static/src/components/App/index.tsx
+++ nekoyume/static/src/components/App/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme  from '@material-ui/core/styles/createMuiTheme';
@@ -7,9 +8,11 @@ class App extends React.Component {
 
     render() {
         return (
-            <MuiThemeProvider theme={createMuiTheme()}>
-                Hello world!
-            </MuiThemeProvider>
+            <BrowserRouter>
+                <MuiThemeProvider theme={createMuiTheme()}>
+                    Hello world!
+                </MuiThemeProvider>
+            </BrowserRouter>
         );
     }
 }
