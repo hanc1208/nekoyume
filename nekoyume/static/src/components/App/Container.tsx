207348a620e8c99330e3e9fa92ee0197abe27c48
Chang-soo Han <are@spoqa.com>
2018-08-15 17:13:30 +0900

    Add AppContainer

---
+++ nekoyume/static/src/components/App/Container.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+class AppContainer extends React.Component {
+
+    render() {
+        return (
+            <>
+                <CssBaseline />
+                Hello world!
+            </>
+        );
+    }
+}
+
+export default AppContainer;
--- nekoyume/static/src/components/App/index.tsx
+++ nekoyume/static/src/components/App/index.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme  from '@material-ui/core/styles/createMuiTheme';
 
+import AppContainer from './Container';
+
 class App extends React.Component {
 
     render() {
         return (
             <BrowserRouter>
                 <MuiThemeProvider theme={createMuiTheme()}>
-                    Hello world!
+                    <AppContainer />
                 </MuiThemeProvider>
             </BrowserRouter>
         );
